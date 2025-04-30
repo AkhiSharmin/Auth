0abe7cdd5674b6d1954ad25587392314d3565b22
AkhiSharmin <akhisharmin100@gmail.com>
2025-04-30 19:10:38 +0600

    some of update login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,43 @@
 import React from "react";
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col ">
+        <div className="text-center lg:text-left">
+          <h1 className="text-2xl font-bold">Login now!</h1>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label-text">Email</label>
+              <input
+                type="email"
+                className="input input-bordered"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label-text">Password</label>
+              <input
+                type="password"
+                className="input input-bordered"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
